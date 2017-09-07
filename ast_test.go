b4f4b6e0e0024eeeba8d70ead76fb39d54d89474
Robert Cooper <rbcooper@gmail.com>
2017-09-07 15:16:41 -0400

    Add convenience builders

--- ast_test.go
+++ ast_test.go
@@ -1,7 +1,7 @@
 package go_sat_solver
 
 import "testing"
-
+import "reflect"
 
 func TestChildrenCount(t *testing.T) {
 	cases := []struct {
@@ -19,6 +19,28 @@ func TestChildrenCount(t *testing.T) {
 		got := len(c.expr.Children())
 		if got != c.want {
 			t.Errorf("len(Children(%#v)) == %v, want %v", c.expr, got, c.want)
+		}
+	}
+}
+
+func TestAstConvenienceBuilders(t *testing.T) {
+	cases := []struct {
+		expr Expr
+		want Expr
+	}{
+		{and("x1", "x2"), And{[]Expr{Symbol{"x1"}, Symbol{"x2"}}}},
+		{and("x1"), Symbol{"x1"}},
+		{and(), Literal{true}},
+		{or("x1", "x2", "x3"), Or{[]Expr{Symbol{"x1"}, Symbol{"x2"}, Symbol{"x3"}}}},
+		{or("x1"), Symbol{"x1"}},
+		{or(), Literal{false}},
+		{not("x1"), Not{[1]Expr{Symbol{"x1"}}}},
+		{not(true), Not{[1]Expr{Literal{true}}}},
+	}
+	for _, c := range cases {
+		got := c.expr
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("Builder made %#v, want %#v", got, c.want)
 		}
 	}
 }
