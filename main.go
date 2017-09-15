d02a0612f130e0ff5ee7a7f25c257b1aed71dce1
John Brattin <john.brattin@gmail.com>
2017-09-14 21:40:43 -0400

    instantiate and print out 2d array

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func main() {
@@ -17,22 +18,35 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	var expression [][]int
+
+	var clauseIndex = 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		switch firstChar := line[:1]; firstChar {
 		case "c":
 			// ignore comment lines
 		case "p":
+			// read numVars and numClauses from p(repare?) line
 			tokens := strings.Split(line, " ")
-			numVars := tokens[2]
-			numClauses := tokens[3]
-			fmt.Println("numVars:", numVars)
-			fmt.Println("numClauses:", numClauses)
+			totalNumVars, _ := strconv.Atoi(tokens[2])
+			totalNumClauses, _ := strconv.Atoi(tokens[3])
+			fmt.Println("totalNumVars:", totalNumVars)
+			fmt.Println("totalNumClauses:", totalNumClauses)
+			expression = make([][]int, totalNumClauses)
 		default:
+			// read variables into a list of lists, representing an AND of OR clauses
 			tokens := strings.Split(line, " ")
-			for i := 0; i < len(tokens)-1; i++ {
-				fmt.Println(tokens[i])
+			numVars := len(tokens)-1
+			expression[clauseIndex] = make([]int, numVars)
+			for varIndex := 0; varIndex < numVars; varIndex++ {
+				intRepr, _ := strconv.Atoi(tokens[varIndex])
+				expression[clauseIndex][varIndex] = intRepr
 			}
+			clauseIndex++
 		}
 	}
+
+	fmt.Println(expression)
 }
